39dfca88d3e09316d76d55138250db6c7c2d80ff
Chutiwat Srisuntorn <chutiwat.s@beryl8.com>
2017-03-09 10:37:55 +0700

    no message

--- main.php
+++ main.php
@@ -9,7 +9,6 @@
     <?php
     // Start the session
     session_start();
-    echo "session_start";
     ?> 
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
